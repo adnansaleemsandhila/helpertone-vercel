f7b6a097fdd0f686868efc679a4ddcde795d42c4
Adnan Saleem Sandhila <adnan.saleem@nestosh.com>
2023-02-22 17:52:52 +0500

    Started setting up authentication with firbase

---
+++ src/configs/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBcxQpdNmzW_e_5Rco_ogC4smrl1NJgd54',
+  authDomain: 'helpertone-583ba.firebaseapp.com',
+  projectId: 'helpertone-583ba',
+  storageBucket: 'helpertone-583ba.appspot.com',
+  messagingSenderId: '658537005532',
+  appId: '1:658537005532:web:4fd155eacc8afb0f7fcf19',
+  measurementId: 'G-DRW6QY6YGB'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -73,6 +73,21 @@ const AuthProvider = ({ children }: Props) => {
   }, [])
 
   const handleLogin = (params: LoginParams, errorCallback?: ErrCallbackType) => {
+    // Default options are marked with *
+    // const response = fetch('http://localhost:3001', {
+    //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //   mode: 'cors', // no-cors, *cors, same-origin
+    //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: 'same-origin', // include, *same-origin, omit
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //     // 'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   redirect: 'follow', // manual, *follow, error
+    //   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    //   body: JSON.stringify(params) // body data type must match "Content-Type" header
+    // })
+
     axios
       .post(authConfig.loginEndpoint, params)
       .then(async response => {
